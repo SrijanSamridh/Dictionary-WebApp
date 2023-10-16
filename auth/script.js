e2ccb9b24052c9c3fdd90537a7fe8e72b7e4ac63
Srijan Samridh <srijansinha3@gmail.com>
2023-10-16 14:41:46 +0530

    Login Signup done

---
+++ auth/script.js
@@ -0,0 +1,33 @@
+const baseUrl = "https://dictionary-server-six.vercel.app";
+const signup_btn = document.getElementById("signup-btn");
+
+signup_btn.addEventListener("click", (event) => {
+  event.preventDefault();
+  const username = document.getElementById("username").value;
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
+  signUp(baseUrl, username, email, password);
+});
+
+// Sending a POST request to the API
+function signUp(baseUrl, username, email, password) {
+  fetch(`${baseUrl}/auth/register`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ username, email, password }),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      alert(data.message);
+      if (data.message === "User registered successfully!") {
+        console.log("Successful");
+        // Redirect to the login page or perform other actions
+        window.location.href = "../routers/login.html";
+      }
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+}
---
+++ dictionary/script.js
@@ -0,0 +1,70 @@
+const url = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const result = document.getElementById("result");
+const sound = document.getElementById("sound");
+
+const btn = document.getElementById("search-btn");
+
+btn.addEventListener("click", () => {
+    let inpWord = document.getElementById("inp-word").value.trim();
+    console.log(inpWord.value);
+    if(!inpWord) {
+      result.innerHTML = `
+          <p class="word-example">
+          Please Enter A word to be searched!
+          </p>`;
+      redirectToIndex();
+    }
+    else{
+      fetch(`${url}${inpWord}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        result.innerHTML = `
+          <div class="word">
+              <h3>${inpWord}</h3>
+              <button onclick="playSound()" id="speak">
+              <i class="fa-solid fa-volume-high" style="color: #b18cfe"></i>
+              </button>
+          </div>
+          <div class="details">
+              <p>${data[0].meanings[0].partOfSpeech}</p>
+              <p>${data[0].phonetic}</p>
+          </div>
+          <p class="word-meaning">
+              ${data[0].meanings[0].definitions[0].definition}
+          </p>
+          <p class="word-example">
+              ${data[0].meanings[0].definitions[0].example || " "}
+          </p>`;
+        // check if sound is available
+        if (data[0].phonetics[0].audio) {
+          sound.setAttribute("src", `${data[0].phonetics[0].audio}`);
+        } else {
+          // if sound is not available hide the sound button
+          document.getElementById("speak").style.display = "none";
+        }
+        console.log(sound);
+      })
+      .catch(() => {
+        result.innerHTML = `
+          <div class="word">
+              <h3>${inpWord}</h3>
+          </div>
+          <div class="details">
+              <p>Not Found</p>
+          </div>
+          <p class="word-meaning">
+              No meaning found
+          </p>
+          <p class="word-example">
+              No example found
+          </p>`;
+      });
+    }
+  });
+  
+  
+
+function playSound() {
+  sound.play();
+}
